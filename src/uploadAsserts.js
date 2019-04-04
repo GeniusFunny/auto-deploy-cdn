04bbc8b4ca26610dd5a48b9ecf4df566d150dc26
Genius Funny <geniusfunnyyh@gmail.com>
2019-04-04 17:40:08 +0800

    'test'

--- src/uploadAsserts.js
+++ src/uploadAsserts.js
@@ -1,6 +1,6 @@
 const { spawnSync } = require('child_process')
 
 const git_add = spawnSync('git', ['add', '.'])
-console.log(git_add.stderr.toString())
 const git_commit = spawnSync('git', ['commit', '-m', `'test'`])
+console.log(git_commit.toString())
 // const git_push = spawnSync('git', ['push', '-u', 'origin', 'master'])
