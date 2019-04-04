cebe3a20b7a506afe107a995ebad1075f9eea4a1
Genius Funny <geniusfunnyyh@gmail.com>
2019-04-04 17:37:54 +0800

    'test'

--- src/uploadAsserts.js
+++ src/uploadAsserts.js
@@ -1,33 +1,5 @@
-const { spawn } = require('child_process')
+const { spawnSync } = require('child_process')
 
-const git_add = spawn('git', ['add', '.'])
-
-git_add.stdout.on('data', (data) => {
-  console.log('???')
-  const git_commit = spawn('git', ['commit', '-m', `'test'`])
-  git_commit.stdout.on('data', data => {
-    const git_push = spawn('git', ['push', '-u', 'origin', 'master'])
-    git_push.stdout.on('data', data => {
-      console.log(data)
-    })
-    git_push.stderr.on('data', err => {
-      console.log(err)
-    })
-    git_push.on('close', info => {
-      console.log('git push close')
-    })
-  })
-  git_commit.stderr.on('data', err => {
-    console.log(err)
-  })
-  git_commit.on('close', info => {
-    console.log('git commit close')
-  })
-})
-git_add.stderr.on('data', err => {
-  console.log(err)
-})
-git_add.on('close', info => {
-  console.log('git add close', info)
-})
-
+const git_add = spawnSync('git', ['add', '.'])
+const git_commit = spawnSync('git', ['commit', '-m', `'test'`])
+const git_push = spawnSync('git', ['push', '-u', 'origin', 'master'])
