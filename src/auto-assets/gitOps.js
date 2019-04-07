816b4a1b4a7c9ef373a029d0438d5c58d08b47d6
Genius Funny <geniusfunnyyh@gmail.com>
2019-04-07 19:00:45 +0800

    测试！！！

--- src/auto-assets/gitOps.js
+++ src/auto-assets/gitOps.js
@@ -45,11 +45,11 @@ function gitCommit(commitMessage = Math.floor(Math.random() * Date.now())) {
   console.log(git_commit)
   console.log(git_commit.stderr.toString())
   console.log(git_commit.stdout.toString())
-  if (git_commit.stderr && git_commit.stderr.length) {
-    // throw Error(git_commit.stderr.toString())
-  } else if (git_commit.stdout && git_commit.stdout.toString().indexOf('nothing to commit, working tree clean') !== -1) {
-    throw Error('nothing to commit, working tree clean')
-  }
+  // if (git_commit.stderr && git_commit.stderr.length) {
+  //   // throw Error(git_commit.stderr.toString())
+  // } else if (git_commit.stdout && git_commit.stdout.toString().indexOf('nothing to commit, working tree clean') !== -1) {
+  //   throw Error('nothing to commit, working tree clean')
+  // }
 }
 
 function gitPush(branch = 'master') {
