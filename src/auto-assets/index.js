8b76f878f836de80691c11c867cb3159c09b538f
Genius Funny <geniusfunnyyh@gmail.com>
2019-04-07 19:51:42 +0800

    连环测试

--- src/auto-assets/index.js
+++ src/auto-assets/index.js
@@ -8,43 +8,43 @@ function autoAssets(svnRemote) {
   const tempDir = `${cwd}/temp`
   const buildDir = `${cwd}/build`
   const distDir = `${cwd}/dist`
-  try {
-    mkdir(tempDir)
-  } catch (e) {
-    console.log('创建临时目录失败', e)
-    throw e
-  }
-  try {
-    svnCoResource(svnRemote, tempDir)
-  } catch (e) {
-    console.log('svn checkout 失败', e)
-    throw e
-  }
-  try {
-    moveFiles(`${buildDir}/assets`, `${tempDir}/yoj`)
-  } catch (e) {
-    console.log('移动静态资源到临时目录失败', e)
-  }
-  try {
-    svnOps('yoj', '测试')
-  } catch (e) {
-    console.log('svn 更新失败', e)
-  }
-  try {
-    deleteFileAndDirectory(tempDir)
-  } catch (e) {
-    console.log('删除临时目录失败', e)
-  }
-  try {
-    mkdir(distDir)
-  } catch (e) {
-    console.log('创建dist目录失败', e)
-  }
-  try {
-    moveFiles(buildDir, distDir)
-  } catch (e) {
-    console.log('移动剩余资源到dist失败', e)
-  }
+  // try {
+  //   mkdir(tempDir)
+  // } catch (e) {
+  //   console.log('创建临时目录失败', e)
+  //   throw e
+  // }
+  // try {
+  //   svnCoResource(svnRemote, tempDir)
+  // } catch (e) {
+  //   console.log('svn checkout 失败', e)
+  //   throw e
+  // }
+  // try {
+  //   moveFiles(`${buildDir}/assets`, `${tempDir}/yoj`)
+  // } catch (e) {
+  //   console.log('移动静态资源到临时目录失败', e)
+  // }
+  // try {
+  //   svnOps('yoj', '测试')
+  // } catch (e) {
+  //   console.log('svn 更新失败', e)
+  // }
+  // try {
+  //   deleteFileAndDirectory(tempDir)
+  // } catch (e) {
+  //   console.log('删除临时目录失败', e)
+  // }
+  // try {
+  //   mkdir(distDir)
+  // } catch (e) {
+  //   console.log('创建dist目录失败', e)
+  // }
+  // try {
+  //   moveFiles(buildDir, distDir)
+  // } catch (e) {
+  //   console.log('移动剩余资源到dist失败', e)
+  // }
   try {
     deleteFileAndDirectory(buildDir)
   } catch (e) {
