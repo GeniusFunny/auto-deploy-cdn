42f5775fb9efb8e783aad613a3828a05c5140a93
Genius Funny <geniusfunnyyh@gmail.com>
2019-04-10 20:48:44 +0800

    去除测试数据

--- test/constants/images.js
+++ test/constants/images.js
@@ -1,19 +1,13 @@
-const avatarJpg = 'http://oimageb2.ydstatic.com/image?id=-6968118995364706265&product=xue'
-const avatarPng = 'http://oimagea2.ydstatic.com/image?id=279992837362562228&product=xue'
-const bg2Jpg = 'http://oimageb3.ydstatic.com/image?id=2206251937531009032&product=xue'
-const bgJpg = 'http://oimagea8.ydstatic.com/image?id=2754198514967770399&product=xue'
-const bgPng = 'http://oimagea5.ydstatic.com/image?id=-4443083210960089490&product=xue'
-const browserStack = 'http://oimageb3.ydstatic.com/image?id=4873895754681321685&product=xue'
-const dailyPic = 'http://oimagec1.ydstatic.com/image?id=-6576632076644194086&product=xue'
-const defaultNewBlog = 'http://oimagec3.ydstatic.com/image?id=-7776593539601089378&product=xue'
+const avatarJpg = '../images/avatar.jpg'
+const avatarPng = '../images/avatar.png'
+const bg2Jpg = '../images/bg 2.jpg'
+const bgJpg = '../images/bg.jpg'
+const bgPng = '../images/bg.png'
 
 export {
   avatarJpg,
   avatarPng,
   bg2Jpg,
   bgJpg,
-  bgPng,
-  browserStack,
-  dailyPic,
-  defaultNewBlog
+  bgPng
 }
